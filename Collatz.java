b731014ae30d59f5be8705ea1e47578c554e8a2b
Shira Goldstein <shira.goldstein@post.runi.ac.il>
2025-11-11 23:32:14 +0200

    Completed assignment

--- Collatz.java
+++ Collatz.java
@@ -4,16 +4,17 @@ public class Collatz {
 		int N = Integer.parseInt(args[0]);
 		String mode = args[1];	
 		int count = 0;
-		int seed = N;
+		int seed = 1;
 		boolean letter = mode.equals("v");
-		while (letter && seed !=1)
-	{
-		for (int i = 1; i <= N; i++)
-		 {
-			seed = i;
-			count++;
-			System.out.print(seed + " ");
-		
+	for (int j = 1; j <= N; j++)
+		{
+		seed = j;
+		if (letter)
+		{
+		System.out.print(seed + " ");
+		count++;
+		do
+		{
 		if (seed%2 == 0)
 		{
 			seed = seed/2;
@@ -22,17 +23,26 @@ public class Collatz {
 		{
 			seed = (seed*3)+1;
 		}
+		if (letter)
+		{
 		System.out.print(seed + " ");	
+		}
 		count++;
-	}
+		}
+		while (seed != 1);
+		
 		System.out.print("(" + count + ")")	;
 		System.out.println("");
-
-	}	
+		count = 0;
+		}}
+		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+	}
+		
+		
 
 }
 
 
-}
+
 
 
